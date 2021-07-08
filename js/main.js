ca23cdc7872b5a479325136f4a215d9fd34bc070
anuarxD <anuar.rodriguez.88@hotmail.com>
2021-07-07 20:23:40 -0500

    07-07-2021
    Avance del día 07-07-2021

---
+++ js/main.js
@@ -0,0 +1,17 @@
+//Tipos de variables
+
+var texto = "Hola, como estas?" //texto
+var x = 20 //Numerico
+var flaf = true; //boolean
+
+// #bot
+var start = ["Hola", "que onda", "hey", "Hello"];
+var chat= ["a si", ["a","b","c"], "apoco"]
+
+function saludar() {
+    alert(start[1])
+}
+
+//alert("Hola, si funciona");
+
+//console.log(texto);
